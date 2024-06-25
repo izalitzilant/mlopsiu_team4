d5749f67703d023526b36b8c01a5271b01ce9dc0
izalitzilant <izalitzilant@gmail.com>
2024-06-25 14:50:13 +0300

    working ...

--- scripts/sample.py
+++ scripts/sample.py
@@ -1,11 +1,16 @@
 # sample 50k records from the dataset
 import pandas as pd
+import hydra
+from omegaconf import DictConfig
 
-def main():
+
+@hydra.main(config_path="configs", config_name="datasets")
+def main(cfg: DictConfig) -> None:
     print('Sampling 50k records from the dataset... ', end='')
-    df = pd.read_csv('../data/train.csv')
+    df = pd.read_csv(cfg.dataset.train_dataset_path)
     df = df.sample(50000)
-    df.to_csv('../data/train50k.csv', index=False)
+    sampled_df_filename = 'train50K'
+    df.to_csv(cfg.dataset.sampled_dataset_path + 'train50K.csv', index=False)
     print('Done!')
 
 if __name__ == "__main__":
